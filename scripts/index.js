ebcea43dbd7a3fc002485ce6aa57ce64d0c96932
Marina Krasnov <marina.krasnova@gmail.com>
2022-01-29 15:02:22 +0200

    js refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,4 @@
 /* Variables */
-const editIcon = document.querySelector('.profile__icon');
-const closeBtn = document.querySelector('.close-btn');
-const overlay = document.querySelector('.overlay');
-const overlayActiveClass = 'overlay_active';
-const title = document.querySelector('.profile__title');
-const subtitle = document.querySelector('.profile__subtitle');
-const popupTitle = document.querySelector('.popup__input_value_name');
-const popupSubtitle = document.querySelector('.popup__input_value_profession');
-const submitButton = document.querySelector('.popup__submit');
-const form = document.querySelector('form');
-
-const overlayAdd = document.querySelector('.overlay-add');
-const addButton = document.querySelector('.profile__add-button');
-
-/*Functions*/
-function openPopup(e) {
-    e.classList.add(overlayActiveClass);
-}
-
-function closePopup(e) {
-    e.classList.remove(overlayActiveClass);
-};
-
-function submit(event) {
-    event.preventDefault();
-    title.textContent = popupTitle.value;
-    subtitle.textContent = popupSubtitle.value;
-    closePopup(overlay);
-}
-
 const initialCards = [{
         name: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -54,70 +24,67 @@ const initialCards = [{
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ]
+const editIcon = document.querySelector('.profile__icon');
+const overlayActiveClass = 'overlay_active';
+const title = document.querySelector('.profile__title');
+const subtitle = document.querySelector('.profile__subtitle');
+const popupTitle = document.querySelector('.popup__input_value_name');
+const popupSubtitle = document.querySelector('.popup__input_value_profession');
+const submitButton = document.querySelector('.popup__submit');
+const formProfile = document.querySelector('[name="form-profile"]');
+const overlayProfile = document.querySelector('.overlay-profile');
+const overlayAdd = document.querySelector('.overlay-add');
+const overlayImageWrapper = document.querySelector('.overlay-image');
+const closeBtn = overlayProfile.querySelector('.close-btn');
+const buttonCloseImage = overlayImageWrapper.querySelector('.close-btn');
+
+const closeBtnAdd = overlayAdd.querySelector('.close-btn');
+const addButton = document.querySelector('.profile__add-button');
 
 const cards = document.querySelector('.cards');
 const cardsTemplate = document.querySelector('#card-template').content.querySelector('.card');
 const formAddCards = document.querySelector('[name="new-place"]');
 const inputPlaceName = document.querySelector('.popup__input_type_place-name');
 const inputLink = document.querySelector('.popup__input_type_link');
-const card = document.querySelector('.card');
-const overlayImageWrapper = document.querySelector('.overlay-image');
 const overlayImage = document.querySelector('.overlay-image__image');
-const closeBtnAdd = overlayAdd.querySelector('.close-btn');
-const renderCard = (item) => {
 
+
+/*Functions*/
+function openPopup(e) {
+    e.classList.add(overlayActiveClass);
+}
+
+function closePopup(e) {
+    e.classList.remove(overlayActiveClass);
+}
+
+function submitProfileForm(event) {
+    event.preventDefault();
+    title.textContent = popupTitle.value;
+    subtitle.textContent = popupSubtitle.value;
+    closePopup(overlayProfile);
+}
+
+// Adding new card to the cards
+function renderCard(card) {
+    cards.prepend(card);
 };
-
-const handlerAddCard = (e) => {
-    e.preventDefault();
+// Creating a card
+function createCard(name, link) {
     const cardElement = cardsTemplate.cloneNode(true);
-    cardElement.querySelector('.card__image').src = inputLink.value;
-    cardElement.querySelector('.card__text').textContent = inputPlaceName.value;
-
-    const handleImageClick = (e) => {
-        openPopup(overlayImageWrapper);
-        const img = e.target.src;
-        overlayImageWrapper.querySelector('.overlay-image__image').src = img;
-        const overlayImageCapture = overlayImageWrapper.querySelector('.overlay-image__capture');
-        overlayImageCapture.textContent = cardElement.querySelector('.card__text').textContent;
-        const buttonCloseImage = overlayImageWrapper.querySelector('.close-btn');
-        buttonCloseImage.addEventListener('click', () => closePopup(overlayImageWrapper));
-    }
-    cardElement.querySelector('.card__image').addEventListener('click', handleImageClick);
-
-    const buttonLike = cardElement.querySelector('.card__button-like');
-    buttonLike.addEventListener('click', () => {
-        buttonLike.classList.toggle('card__button-like_active')
-    });
-    const buttonBin = cardElement.querySelector('.card__button-delete');
-    buttonBin.addEventListener('click', () => {
-        cardElement.remove();
-    });
-    //Button to close popup
-    const buttonCloseImage = overlayImageWrapper.querySelector('.close-btn');
-    buttonCloseImage.addEventListener('click', closePopup(overlayImageWrapper));
-
-    cards.prepend(cardElement);
-    closePopup(overlayAdd);
-};
-// Adding a card from the list
-initialCards.forEach(item => {
-    const cardElement = cardsTemplate.cloneNode(true);
-    cardElement.querySelector('.card__image').src = item.link;
-    cardElement.querySelector('.card__text').textContent = item.name;
+    cardElement.querySelector('.card__text').textContent = name;
+    cardElement.querySelector('.card__image').alt = `Виды на ${name}`;
+    cardElement.querySelector('.card__image').src = link;
     //Creating popup for each card
     const handleImageClick = (e) => {
         openPopup(overlayImageWrapper);
         const img = e.target.src;
-        overlayImageWrapper.querySelector('.overlay-image__image').src = img;
+        overlayImage.src = img;
         const overlayImageCapture = overlayImageWrapper.querySelector('.overlay-image__capture');
         overlayImageCapture.textContent = cardElement.querySelector('.card__text').textContent;
-        //Button to close popup
-        const buttonCloseImage = overlayImageWrapper.querySelector('.close-btn');
-        buttonCloseImage.addEventListener('click', () => closePopup(overlayImageWrapper));
     }
     cardElement.querySelector('.card__image').addEventListener('click', handleImageClick);
-    //Hanging button "Like" on a card
+    //Hanging a button "Like" on a card
     const buttonLike = cardElement.querySelector('.card__button-like');
     buttonLike.addEventListener('click', () => {
         buttonLike.classList.toggle('card__button-like_active')
@@ -127,30 +94,34 @@ initialCards.forEach(item => {
     buttonBin.addEventListener('click', () => {
         cardElement.remove();
     });
-    //Adding a card to cards
-    cards.prepend(cardElement);
+    return cardElement;
+};
+// Adding a card from the list
+initialCards.forEach(item => {
+    nameElement = item.name;
+    linkElement = item.link;
+    const card = createCard(nameElement, linkElement);
+    renderCard(card);
 });
-form.addEventListener('submit', submit);
+// Adding a card from the form
+const handlerAddCard = (e) => {
+    e.preventDefault();
+    nameCard = inputPlaceName.value;
+    linkCard = inputLink.value;
+    const card = createCard(nameCard, linkCard);
+    renderCard(card);
+    inputLink.value = "";
+    inputPlaceName.value = "";
+    closePopup(overlayAdd);
+};
+formProfile.addEventListener('submit', submitProfileForm);
 formAddCards.addEventListener('submit', handlerAddCard);
+addButton.addEventListener('click', () => openPopup(overlayAdd));
 editIcon.addEventListener('click', (e) => {
     popupTitle.value = title.textContent;
     popupSubtitle.value = subtitle.textContent;
-    openPopup(overlay);
+    openPopup(overlayProfile);
 });
-closeBtn.addEventListener('click', () => closePopup(overlay));
+closeBtn.addEventListener('click', () => closePopup(overlayProfile));
 closeBtnAdd.addEventListener('click', () => closePopup(overlayAdd));
-
-addButton.addEventListener('click', () => openPopup(overlayAdd));
-
-//Closing Popup with a help of escape button
-const closeAll = () => {
-    closePopup(overlay);
-    closePopup(overlayAdd);
-    closePopup(overlayImageWrapper);
-};
-
-document.addEventListener('keydown', function (e) {
-    if (e.code === 'Escape') {
-        closeAll();
-    }
-});
+buttonCloseImage.addEventListener('click', () => closePopup(overlayImageWrapper));
